a29816461e2a4d2ee1356dcf8c7b9142d3a8eb28
violet <569252295@qq.com>
2020-05-17 19:16:54 +0800

    新建项目

---
+++ spring-cloud-config/src/main/java/application/ConfigServerApplication.java
@@ -0,0 +1,24 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <p>
+ * TODO
+ * </p>
+ *
+ * @author violet
+ * @version 1.0
+ * @since 5/17/2020 17:09
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
